98d19ca5da2542fd6f81d3ff411c22be78bb0538
Bành Hảo Toàn <156346392+dtnamfitus@users.noreply.github.com>
2024-11-28 23:09:28 +0700

    product detail

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,7 +4,7 @@ const getProductById = async (req, res) => {
   try {
     const { id } = req.params;
     const product = await productService.getProductById(id);
-    res.status(200).json(product);
+    return product;
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -13,7 +13,7 @@ const getProductById = async (req, res) => {
 const getProducts = async (req, res) => {
   try {
     const products = await productService.getAllProducts(req);
-    res.status(200).json(products);
+    return products;
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
